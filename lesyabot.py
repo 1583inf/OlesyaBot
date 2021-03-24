b41e4ab380793071a095e144c246803e5e47d1cb
1583inf <79850708+1583inf@users.noreply.github.com>
2021-03-24 17:50:03 +0300

    Update lesyabot.py

--- lesyabot.py
+++ lesyabot.py
@@ -1,5 +1,5 @@
 import telebot
-bot = telebot.TeleBot('1512568206:AAHySA7FLNJV8pLVfo8sW6YoLqoiRS4Du58 ')
+bot = telebot.TeleBot('1512568206:AAHySA7FLNJV8pLVfo8sW6YoLqoiRS4Du58')
 @bot.message_handler(commands=['start', 'help'])
 def sens_welcome(message):
     bot.reply_to(message, 'Я бот, Приятно познакомиться,  {message.from_user.first_name)')
